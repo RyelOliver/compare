af0e0579b0dfa5aa4f4b5c88f29c45f7e37f5d50
Ryel Oliver <rmuchunku@gmail.com>
2019-04-30 22:18:55 +0100

    longestUnmodified API

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ const status = {
 };
 
 const characters = {
+    longestUnmodified: {
+        of: longerValue => ({
+            with: shorterValue => {
+                // See if the shorterValue exists within the longerValue
+                if (longerValue.includes(shorterValue))
+                    return shorterValue;
+                // If it doesn't and it's one character, then there's no unmodified string
+                if (shorterValue.length === 1)
+                    return '';
+            
+                // See if a substring of the shorterValue exists within the longerValue
+                for (let shortenBy = 1; shortenBy < shorterValue.length; shortenBy++) {
+                    // Try substrings with one less character until one exists within the longerValue
+                    for (let startChar = 0; startChar <= shortenBy; startChar++) {
+                        const endChar = shorterValue.length - (shortenBy - startChar);
+                        const evenshorterValue = shorterValue.substring(startChar, endChar);
+            
+                        if (longerValue.includes(evenshorterValue))
+                            return evenshorterValue;
+                    }
+                }
+                
+                return '';
+            },
+        }),
+    },
     of: oldValue => ({
         with: newValue => {
             if (typeof oldValue !== 'string' || typeof newValue !== 'string')
--- index.test.js
+++ index.test.js
@@ -2,6 +2,43 @@ const compare = require('./index');
 
 describe('Compare characters', () => {
 
+    describe('Longest unmodified', () => {
+        test('No text that is the same', () => {
+            const actual = compare.characters.longestUnmodified
+                .of('Lorem ipsum').with('x');
+            const expected = '';
+            expect(actual).toEqual(expected);
+        });
+
+        test('The first part of the text is the same', () => {
+            const actual = compare.characters.longestUnmodified
+                .of('Lorem ipsum').with('Lorexxx');
+            const expected = 'Lore';
+            expect(actual).toEqual(expected);
+        });
+
+        test('The last part of the text is the same', () => {
+            const actual = compare.characters.longestUnmodified
+                .of('Lorem ipsum').with('xxxpsum');
+            const expected = 'psum';
+            expect(actual).toEqual(expected);
+        });
+
+        test('The middle part of the text is the same', () => {
+            const actual = compare.characters.longestUnmodified
+                .of('Lorem ipsum').with('xxxrem ipsum');
+            const expected = 'rem ipsum';
+            expect(actual).toEqual(expected);
+        });
+
+        test('Multiple middle parts of the text', () => {
+            const actual = compare.characters.longestUnmodified
+                .of('Lorem ipsum dolor sit').with('Lorex ipxum doxor xit');
+            const expected = 'um do';
+            expect(actual).toEqual(expected);
+        });
+    });
+
     describe('Errors for values that aren\'t strings', () => {
         test('Booleans', () => {
             expect(() => compare.characters.of(true).with(true)).toThrow();
