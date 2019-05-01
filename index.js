3f405ef81672d6ed14e061656f32528dcc7a0b84
Ryel Oliver <rmuchunku@gmail.com>
2019-05-02 00:44:36 +0100

    words API

--- index.js
+++ index.js
@@ -69,28 +69,17 @@ const characters = {
                     const newUnmodifiedValueStart = newValue.indexOf(unmodifiedValue);
                     const beforeNewUnmodifiedValue = newValue.substring(0, newUnmodifiedValueStart);
                     const afterNewUnmodifiedValue = newValue.substring(newUnmodifiedValueStart + unmodifiedValueLength);
+                    const beforeOldUnmodifiedValue = oldValue.substring(0, oldUnmodifiedValueStart);
+                    const afterOldUnmodifiedValue = oldValue.substring(oldUnmodifiedValueStart + unmodifiedValueLength);
             
-                    // If this is all of the oldValue, before and after of the newValue is all added
-                    if (oldUnmodifiedValueStart === 0 && unmodifiedValueLength === oldValueLength) {
-                        if (beforeNewUnmodifiedValue)
-                            compared.push({ status: added, value: beforeNewUnmodifiedValue });
-                
-                        if (unmodifiedValue)
-                            compared.push({ status: unmodified, value: unmodifiedValue });
-                        
-                        if (afterNewUnmodifiedValue)
-                            compared.push({ status: added, value: afterNewUnmodifiedValue });
-                    } else {
-                        const beforeOldUnmodifiedValue = oldValue.substring(0, oldUnmodifiedValueStart);
-                        const afterOldUnmodifiedValue = oldValue.substring(oldUnmodifiedValueStart + unmodifiedValueLength);
-            
+                    if (beforeOldUnmodifiedValue || beforeNewUnmodifiedValue)
                         compared = compared.concat(characters.of(beforeOldUnmodifiedValue).with(beforeNewUnmodifiedValue));
             
-                        if (unmodifiedValue)
-                            compared.push({ status: unmodified, value: unmodifiedValue });
+                    if (unmodifiedValue)
+                        compared.push({ status: unmodified, value: unmodifiedValue });
                         
+                    if (afterOldUnmodifiedValue || afterNewUnmodifiedValue)
                         compared = compared.concat(characters.of(afterOldUnmodifiedValue).with(afterNewUnmodifiedValue));
-                    }
                 }
             // If oldValue is longer, something was removed
             } else {
@@ -105,28 +94,17 @@ const characters = {
                     const oldUnmodifiedValueStart = oldValue.indexOf(unmodifiedValue);
                     const beforeOldUnmodifiedValue = oldValue.substring(0, oldUnmodifiedValueStart);
                     const afterOldUnmodifiedValue = oldValue.substring(oldUnmodifiedValueStart + unmodifiedValueLength);
-                
-                    // If this is all of the newValue, before and after of the oldValue is all removed
-                    if (newUnmodifiedValueStart === 0 && unmodifiedValueLength === newValueLength) {
-                        if (beforeOldUnmodifiedValue)
-                            compared.push({ status: removed, value: beforeOldUnmodifiedValue });
+                    const beforeNewUnmodifiedValue = newValue.substring(0, newUnmodifiedValueStart);
+                    const afterNewUnmodifiedValue = newValue.substring(newUnmodifiedValueStart + unmodifiedValueLength);
         
-                        if (unmodifiedValue)
-                            compared.push({ status: unmodified, value: unmodifiedValue });
-                    
-                        if (afterOldUnmodifiedValue)
-                            compared.push({ status: removed, value: afterOldUnmodifiedValue });
-                    } else {
-                        const beforeNewUnmodifiedValue = newValue.substring(0, newUnmodifiedValueStart);
-                        const afterNewUnmodifiedValue = newValue.substring(newUnmodifiedValueStart + unmodifiedValueLength);
-        
+                    if (beforeOldUnmodifiedValue || beforeNewUnmodifiedValue)
                         compared = compared.concat(characters.of(beforeOldUnmodifiedValue).with(beforeNewUnmodifiedValue));
         
-                        if (unmodifiedValue)
-                            compared.push({ status: unmodified, value: unmodifiedValue });
+                    if (unmodifiedValue)
+                        compared.push({ status: unmodified, value: unmodifiedValue });
                     
+                    if (afterOldUnmodifiedValue || afterNewUnmodifiedValue)
                         compared = compared.concat(characters.of(afterOldUnmodifiedValue).with(afterNewUnmodifiedValue));
-                    }
                 }
             }
         
@@ -135,7 +113,118 @@ const characters = {
     }),
 };
 
+const words = {
+    longestUnmodified: {
+        of: longerValue => ({
+            with: shorterValue => {
+                // See if the shorterValue exists within the longerValue
+                if (longerValue.includes(shorterValue))
+                    return shorterValue;
+
+                // Could include an option to group whitespace, /(\s+)/
+                // Could include an option to ignore whitespace, /\s+/
+                const shorterValueList = shorterValue.split(/(\s)/);
+
+                // If it doesn't and it's one word, then there's no unmodified string
+                if (shorterValueList.length === 1)
+                    return '';
+            
+                // See if a substring of the shorterValue exists within the longerValue
+                for (let shortenBy = 1; shortenBy < shorterValueList.length; shortenBy++) {
+                    // Try substrings with one less word until one exists within the longerValue
+                    for (let startWord = 0; startWord <= shortenBy; startWord++) {
+                        const endWord = shorterValueList.length - (shortenBy - startWord);
+                        const evenshorterValue = shorterValueList.slice(startWord, endWord).join('');
+            
+                        if (longerValue.includes(evenshorterValue))
+                            return evenshorterValue;
+                    }
+                }
+                
+                return '';
+            },
+        }),
+    },
+    of: oldValue => ({
+        with: newValue => {
+            if (typeof oldValue !== 'string' || typeof newValue !== 'string')
+                throw new TypeError('Two strings must be provided for comparisons');
+
+            if (!oldValue && !newValue)
+                return [{ status: unmodified, value: '' }];
+
+            if (!oldValue)
+                return [{ status: added, value: newValue }];
+            
+            if (!newValue)
+                return [{ status: removed, value: oldValue }];
+
+            if (newValue === oldValue)
+                return [{ status: unmodified, value: newValue }];
+            
+            let compared = [];
+            const oldValueLength = oldValue.length;
+            const newValueLength = newValue.length;
+        
+            // If newValue is longer, something was added
+            if (newValueLength >= oldValueLength) {
+                const unmodifiedValue = words.longestUnmodified.of(newValue).with(oldValue);
+        
+                if (!unmodifiedValue) {
+                    compared.push({ status: removed, value: oldValue });
+                    compared.push({ status: added, value: newValue });
+                } else {
+                    const unmodifiedValueLength = unmodifiedValue.length;
+                    const oldUnmodifiedValueStart = oldValue.indexOf(unmodifiedValue);
+                    const newUnmodifiedValueStart = newValue.indexOf(unmodifiedValue);
+                    const beforeNewUnmodifiedValue = newValue.substring(0, newUnmodifiedValueStart);
+                    const afterNewUnmodifiedValue = newValue.substring(newUnmodifiedValueStart + unmodifiedValueLength);
+                    const beforeOldUnmodifiedValue = oldValue.substring(0, oldUnmodifiedValueStart);
+                    const afterOldUnmodifiedValue = oldValue.substring(oldUnmodifiedValueStart + unmodifiedValueLength);
+                        
+                    if (beforeOldUnmodifiedValue || beforeNewUnmodifiedValue)
+                        compared = compared.concat(words.of(beforeOldUnmodifiedValue).with(beforeNewUnmodifiedValue));
+            
+                    if (unmodifiedValue)
+                        compared.push({ status: unmodified, value: unmodifiedValue });
+
+                    if (afterOldUnmodifiedValue || afterNewUnmodifiedValue)
+                        compared = compared.concat(words.of(afterOldUnmodifiedValue).with(afterNewUnmodifiedValue));
+                }
+            // If oldValue is longer, something was removed
+            } else {
+                const unmodifiedValue = words.longestUnmodified.of(oldValue).with(newValue);
+            
+                if (!unmodifiedValue) {
+                    compared.push({ status: removed, value: oldValue });
+                    compared.push({ status: added, value: newValue });
+                } else {
+                    const unmodifiedValueLength = unmodifiedValue.length;
+                    const newUnmodifiedValueStart = newValue.indexOf(unmodifiedValue);
+                    const oldUnmodifiedValueStart = oldValue.indexOf(unmodifiedValue);
+                    const beforeOldUnmodifiedValue = oldValue.substring(0, oldUnmodifiedValueStart);
+                    const afterOldUnmodifiedValue = oldValue.substring(oldUnmodifiedValueStart + unmodifiedValueLength);
+                    const beforeNewUnmodifiedValue = newValue.substring(0, newUnmodifiedValueStart);
+                    const afterNewUnmodifiedValue = newValue.substring(newUnmodifiedValueStart + unmodifiedValueLength);
+            
+                    if (beforeOldUnmodifiedValue || beforeNewUnmodifiedValue)
+                        compared = compared.concat(words.of(beforeOldUnmodifiedValue).with(beforeNewUnmodifiedValue));
+            
+                    if (unmodifiedValue)
+                        compared.push({ status: unmodified, value: unmodifiedValue });
+                        
+                    if (afterOldUnmodifiedValue || afterNewUnmodifiedValue)
+                        compared = compared.concat(words.of(afterOldUnmodifiedValue).with(afterNewUnmodifiedValue));
+                }
+            }
+
+            return compared;
+        },
+    }),
+};
+
 module.exports = {
     status,
     characters,
+    words,
 };
--- index.test.js
+++ index.test.js
@@ -1,7 +1,6 @@
 const compare = require('./index');
 
 describe('Compare characters', () => {
-
     describe('Longest unmodified', () => {
         test('No text that is the same', () => {
             const actual = compare.characters.longestUnmodified
@@ -166,6 +165,196 @@ describe('Compare characters', () => {
                 { status: compare.status.removed, value: 'dolor ' },
                 { status: compare.status.unmodified, value: 'sit' },
                 { status: compare.status.removed, value: ' amet' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+    });
+});
+
+describe('Compare words', () => {
+    describe('Longest unmodified', () => {
+        test('No words that are the same', () => {
+            const actual = compare.words.longestUnmodified
+                .of('Lorem ipsum').with('x');
+            const expected = '';
+            expect(actual).toEqual(expected);
+        });
+
+        test('The first word is the same', () => {
+            const actual = compare.words.longestUnmodified
+                .of('Lorem ipsum').with('Lorem xxx');
+            const expected = 'Lorem ';
+            expect(actual).toEqual(expected);
+        });
+
+        test('The last word is the same', () => {
+            const actual = compare.words.longestUnmodified
+                .of('Lorem ipsum').with('xxx ipsum');
+            const expected = ' ipsum';
+            expect(actual).toEqual(expected);
+        });
+
+        test('A word in the middle is the same', () => {
+            const actual = compare.words.longestUnmodified
+                .of('Lorem ipsum dolor').with('xxx ipsum xxx');
+            const expected = ' ipsum ';
+            expect(actual).toEqual(expected);
+        });
+
+        test('Multiple words in the middle are the same', () => {
+            const actual = compare.words.longestUnmodified
+                .of('Lorem ipsum dolor sit amet').with('ipsum sit');
+            const expected = 'ipsum ';
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe('Errors for values that aren\'t strings', () => {
+        test('Booleans', () => {
+            expect(() => compare.words.of(true).with(true)).toThrow();
+        });
+        test('Numbers', () => {
+            expect(() => compare.words.of(1).with(1)).toThrow();
+        });
+        test('Objects', () => {
+            expect(() => compare.words.of({ key: 1 }).with({ key: 1 })).toThrow();
+        });
+    });
+
+    test('Two strings that are the same', () => {
+        const actual = compare.words
+            .of('Lorem ipsum').with('Lorem ipsum');
+        const expected = [{ status: compare.status.unmodified, value: 'Lorem ipsum' }];
+        expect(actual).toStrictEqual(expected);
+    });
+
+    test('Two strings that are both empty', () => {
+        const actual = compare.words
+            .of('').with('');
+        const expected = [{ status: compare.status.unmodified, value: '' }];
+        expect(actual).toStrictEqual(expected);
+    });
+
+    describe('With two strings where the last has additions', () => {
+        test('To the whole string', () => {
+            const actual = compare.words
+                .of('').with('Lorem ipsum');
+            const expected = [
+                { status: compare.status.added, value: 'Lorem ipsum' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('At the start of the string', () => {
+            const actual = compare.words
+                .of('ipsum').with('Lorem ipsum');
+            const expected = [
+                { status: compare.status.added, value: 'Lorem ' },
+                { status: compare.status.unmodified, value: 'ipsum' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('At the end of the string', () => {
+            const actual = compare.words
+                .of('Lorem').with('Lorem ipsum');
+            const expected = [
+                { status: compare.status.unmodified, value: 'Lorem' },
+                { status: compare.status.added, value: ' ipsum' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('In multiple parts of the string', () => {
+            const actual = compare.words
+                .of('Lorem dolor amet').with('Lorem ipsum dolor sit amet');
+            const expected = [
+                { status: compare.status.unmodified, value: 'Lorem' },
+                { status: compare.status.added, value: ' ipsum' },
+                { status: compare.status.unmodified, value: ' dolor ' },
+                { status: compare.status.added, value: 'sit ' },
+                { status: compare.status.unmodified, value: 'amet' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('Of white space', () => {
+            const actual = compare.words
+                .of('Lorem ipsum dolor sit amet').with('Lorem ipsum  dolor  sit amet ');
+            const expected = [
+                { status: compare.status.unmodified, value: 'Lorem ipsum ' },
+                { status: compare.status.added, value: ' ' },
+                { status: compare.status.unmodified, value: 'dolor' },
+                { status: compare.status.added, value: ' ' },
+                { status: compare.status.unmodified, value: ' sit amet' },
+                { status: compare.status.added, value: ' ' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+    });
+
+    describe('With two strings where the last has removals', () => {
+        test('To the whole string', () => {
+            const actual = compare.words
+                .of('Lorem ipsum').with('');
+            const expected = [{ status: compare.status.removed, value: 'Lorem ipsum' }];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('At the start of the string', () => {
+            const actual = compare.words
+                .of('Lorem ipsum').with('ipsum');
+            const expected = [
+                { status: compare.status.removed, value: 'Lorem ' },
+                { status: compare.status.unmodified, value: 'ipsum' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('At the end of the string', () => {
+            const actual = compare.words
+                .of('Lorem ipsum').with('Lorem');
+            const expected = [
+                { status: compare.status.unmodified, value: 'Lorem' },
+                { status: compare.status.removed, value: ' ipsum' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('At the start and end of the string', () => {
+            const actual = compare.words
+                .of('Lorem ipsum dolor').with('ipsum');
+            const expected = [
+                { status: compare.status.removed, value: 'Lorem ' },
+                { status: compare.status.unmodified, value: 'ipsum' },
+                { status: compare.status.removed, value: ' dolor' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('In multiple parts of the string', () => {
+            const actual = compare.words
+                .of('Lorem ipsum dolor sit amet').with('Lorem dolor amet');
+            const expected = [
+                { status: compare.status.unmodified, value: 'Lorem' },
+                { status: compare.status.removed, value: ' ipsum' },
+                { status: compare.status.unmodified, value: ' dolor ' },
+                { status: compare.status.removed, value: 'sit ' },
+                { status: compare.status.unmodified, value: 'amet' },
+            ];
+            expect(actual).toStrictEqual(expected);
+        });
+
+        test('Of white space', () => {
+            const actual = compare.words
+                .of('Lorem ipsum  dolor  sit amet ').with('Lorem ipsum dolor sit amet');
+            const expected = [
+                { status: compare.status.unmodified, value: 'Lorem ipsum ' },
+                { status: compare.status.removed, value: ' ' },
+                { status: compare.status.unmodified, value: 'dolor' },
+                { status: compare.status.removed, value: ' ' },
+                { status: compare.status.unmodified, value: ' sit amet' },
+                { status: compare.status.removed, value: ' ' },
             ];
             expect(actual).toStrictEqual(expected);
         });
